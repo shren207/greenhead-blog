a5072f5d3d604a105bd252ef44ea94845afa0de8
shren207 <shren0812@gmail.com>
2021-12-06 14:54:00 +0900

    reset

---
+++ gatsby-meta-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: `Greenhead Blog`,
+  description: `Blog posted about ...`,
+  author: `Greenhead`,
+  introduction: `녹두의 개발 다이어리`,
+  siteUrl: `https://my-greenhead-blog.netlify.app/`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `shren207`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: 'greenhead', // Your disqus-short-name. check disqus.com.
+    utterances: 'github.com/shren207/my-greenhead-blog', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+  ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
+}

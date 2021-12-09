251eab9d0b033df020df3ecad14267ea1ed331a1
shren207 <shren0812@gmail.com>
2021-12-09 16:42:04 +0900

    reset post-width from 29 to 24

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -18,7 +18,7 @@ export const Layout = ({ location, title, children }) => {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(29), // 기본값은 24, Gparkkii.io의 포스트 폭을 참고함.
+          maxWidth: rhythm(24), // 기본값은 24, Gparkkii.io의 포스트 폭은 29
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >

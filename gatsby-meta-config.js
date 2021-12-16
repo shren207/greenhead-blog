42e9f2f19cb87131f2f7115c465b3705c669ec3e
shren207 <shren0812@gmail.com>
2021-12-16 23:14:28 +0900

    remove disqus, add utterances

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -15,8 +15,8 @@ module.exports = {
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    disqusShortName: 'greenhead', // Your disqus-short-name. check disqus.com.
-    utterances: 'github.com/shren207/my-greenhead-blog', // Your repository for archive comment
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'shren207/my-greenhead-blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post

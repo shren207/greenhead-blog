51448320cd00ce7b21aa65b61a72d52ac80d59f7
Green <shren0812@gmail.com>
2023-12-23 17:08:22 +0900

    feat: gatsby-remark-videos 설치
    markdown 파일에 동영상 표시될 수 있도록 플러그인 설치

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,38 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: `gatsby-remark-videos`,
+            options: {
+              pipelines: [
+                {
+                  name: 'vp9',
+                  transcode: chain =>
+                    chain
+                      .videoCodec('libvpx-vp9')
+                      .noAudio()
+                      .outputOptions(['-crf 20', '-b:v 0']),
+                  maxHeight: 480,
+                  maxWidth: 900,
+                  fileExtension: 'webm',
+                },
+                {
+                  name: 'h264',
+                  transcode: chain =>
+                    chain
+                      .videoCodec('libx264')
+                      .noAudio()
+                      .addOption('-profile:v', 'main')
+                      .addOption('-pix_fmt', 'yuv420p')
+                      .outputOptions(['-movflags faststart'])
+                      .videoBitrate('1000k'),
+                  maxHeight: 480,
+                  maxWidth: 900,
+                  fileExtension: 'mp4',
+                },
+              ],
+            }
+          },
+          {
             resolve: `gatsby-remark-katex`,
             options: {
               strict: `ignore`,

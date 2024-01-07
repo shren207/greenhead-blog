2f14699c024e8ae5abe68922f0107d6389fdf95f
Green <shren0812@gmail.com>
2024-01-07 17:44:19 +0900

    feat: 포스트 description 추가

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -3,10 +3,8 @@ import { Link } from 'gatsby'
 import { TARGET_CLASS } from '../../utils/visible'
 import './index.scss'
 
+// img 파일의 크기는 150px * 150px
 export const ThumbnailItem = ({ node }) => {
-  // console.log(node) 여기로 오는 frontmatter의 값을 확인할 수 있다.
-  // 필요한 경우 '../../pages/index.js' 이 파일의 frontmatter를 수정하자
-  // img 파일의 크기는 150px * 150px
   return (
     <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
       <div className={`container`} key={node.fields.slug}>
@@ -14,7 +12,7 @@ export const ThumbnailItem = ({ node }) => {
           <span className={`tag`}>{node.frontmatter.category}</span>
           <span className={`date`}>{node.frontmatter.date}</span>
           <h3>{node.frontmatter.title || node.fields.slug}</h3>
-          <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+          <p dangerouslySetInnerHTML={{ __html: node.frontmatter.description || node.excerpt }} />
         </div>
         {node.frontmatter.thumbnail ? (
           <img

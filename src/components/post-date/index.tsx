b833861678e2287718ccb8dc77e8cc649b270d50
shren207 <shren0812@gmail.com>
2024-03-11 16:08:57 +0900

    feat: Module SCSS에 `classnames` 라이브러리 적용

--- src/components/post-date/index.tsx
+++ src/components/post-date/index.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from 'react'
+import classNames from 'classnames/bind'
 import styles from './index.module.scss'
+
+const cx = classNames.bind(styles)
 
 interface PostDateProps {
   date: string
 }
 
 export const PostDate: FC<PostDateProps> = ({ date }) => {
-  return <p className={styles.postDate}>{date}</p>
+  return <p className={cx('container')}>{date}</p>
 }

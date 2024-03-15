5ec0b1eee4d573036cfd0c563ae53965f8dbb936
shren207 <shren0812@gmail.com>
2024-03-15 21:53:25 +0900

    feat: Callout 컴포넌트 작성
    - Docusaurus의 Callout UI 참고
      - note
      - tip
      - info
      - warning

--- src/components/mdx/callout/index.tsx
+++ src/components/mdx/callout/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import classNames from 'classnames/bind'
-import styles from './index.module.scss'
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './index.module.scss';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 interface CalloutProps {
-  variant: 'normal' | 'warning' | 'error';
+  variant: 'note' | 'tip' | 'info' | 'warning';
   children: React.ReactNode;
 }
 
-export const Callout: React.FC<CalloutProps> = ({variant = 'normal', children}) => {
-
+export const Callout: React.FC<CalloutProps> = ({
+  variant = 'note',
+  children,
+}) => {
   return (
-   <div className={cx({ container: true, [variant]: true})}>
-     {children}
-   </div>
-  )
-}
+    <div className={cx({ container: true, [variant]: true })}>{children}</div>
+  );
+};
